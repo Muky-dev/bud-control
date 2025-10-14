76c65676403d07e87f969932bddfae5ce43a1ef6
Muky-dev <ti.gabrielfini@gmail.com>
2025-10-14 13:19:58 -0300

    Invoke discover from frontend

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { BudModesRadioGroup } from "@/components/ui/BudModesRadioGroup";
 import { DeviceCard } from "@/components/ui/DeviceCard";
 import { createFileRoute } from "@tanstack/react-router";
+import { invoke } from "@tauri-apps/api/core";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  useEffect(() => {
+    invoke("discover_audio_devices");
+  }, []);
   return (
     <div className="flex flex-col p-4 gap-4 min-h-full justify-between">
       <DeviceCard />
