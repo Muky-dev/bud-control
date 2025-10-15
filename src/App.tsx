b1fff7ac4e5baab55be6c1241097f844b3328e7f
Muky-dev <ti.gabrielfini@gmail.com>
2025-10-14 22:39:05 -0300

    Wrap RouterProvider with QueryClientProvider for react-query integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@/styles/global.css";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -16,7 +18,9 @@ declare module "@tanstack/react-router" {
 function App() {
   return (
     <HeroUIProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HeroUIProvider>
   );
 }
